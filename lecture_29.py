25e3a5f2ff445bc3bba729824538665e406d9b9b
sashakutuzovv <52065001+sashakutuzovv@users.noreply.github.com>
2025-07-06 14:41:13 +0100

    lecture_29.py

--- lecture_29.py
+++ lecture_29.py
@@ -2,6 +2,7 @@ import torch
 import tiktoken
 from lecture_25 import GPTModel, generate_text_simple
 from lecture_26 import GPT_CONFIG_124M, text_to_token_ids, token_ids_to_text
+import matplotlib.pyplot as plt
 
 model = GPTModel(GPT_CONFIG_124M)
 model.load_state_dict(torch.load("gpt124m.pth", map_location="cpu"))
@@ -14,8 +15,66 @@ token_ids = generate_text_simple(
     model=model,
     idx=text_to_token_ids("Every effort moves you", tokenizer),
     max_new_tokens=25,
-    context_size=GPT_CONFIG_124M
+    context_size=GPT_CONFIG_124M["context_length"]
 )
 
 print("Output (default):\n", token_ids_to_text(token_ids, tokenizer))
 
+vocab = {
+    "closer": 0,
+    "every": 1,
+    "effort": 2,
+    "forward": 3,
+    "inches": 4,
+    "moves": 5,
+    "pizza": 6,
+    "toward": 7,
+    "you": 8,
+}
+
+inverse_vocab = {v: k for k, v in vocab.items()}
+next_token_logits = torch.tensor(
+    [4.51, 0.89, -1.90, 6.75, 1.63, -1.62, -1.89, 6.28, 1.79]
+)
+
+probas = torch.softmax(next_token_logits, dim=0)
+print(probas)
+next_token_id = torch.argmax(probas).item()
+print(next_token_id)
+print(inverse_vocab[next_token_id])
+
+torch.manual_seed(123)
+next_token_id = torch.multinomial(probas, num_samples=1).item()
+print(inverse_vocab[next_token_id])
+
+def print_sampled_tokens(probas):
+    torch.manual_seed(123)
+    sample = [torch.multinomial(probas, num_samples=1).item() for i in range(1_000)]
+    sampled_ids = torch.bincount(torch.tensor(sample))
+    for i, freq in enumerate(sampled_ids):
+        print(f"{freq} x {inverse_vocab[i]}")
+
+print_sampled_tokens(probas)
+
+def softmax_with_temperature(logits, temperature):
+    scaled_logits = logits / temperature
+    return torch.softmax(scaled_logits, dim=0)
+
+temperatures = [1, 0.5, 5]
+scaled_probas = [softmax_with_temperature(next_token_logits, T) for T in temperatures]
+
+x = torch.arange(len(vocab))
+bar_width = 0.15
+
+fig, ax = plt.subplots(figsize=(5, 3))
+for i, T in enumerate(temperatures):
+    rects = ax.bar(x + i * bar_width, scaled_probas[i], bar_width, label=f'Temperature = {T}')
+
+ax.set_ylabel('Probability')
+ax.set_xticks(x)
+ax.set_xticklabels(vocab.keys(), rotation=90)
+ax.legend()
+
+plt.tight_layout()
+plt.savefig("temperature-plot.pdf")
+plt.show()
